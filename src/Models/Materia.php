106fe94ad7139fc0c2f039501ab5983bf068bd77
Daniela Castro <daniela.castro@phs.mx>
2023-05-27 20:51:19 -0600

    avances

---
+++ src/Models/Materia.php
@@ -0,0 +1,255 @@
+<?php
+
+require_once "Conexion.php";
+
+class Materia
+{
+    private $materiaId;
+    private $carreraId;
+    private $especialidadId;
+    private $nombre;
+    private $area;
+    private $semestre;
+    private $competencia;
+    private $urlVideo;
+    private $urlPrograma;
+    private $status;
+    private $connection;
+
+    public function setConnection($conn)
+    {
+        $this->connection = $conn;
+    }
+
+
+    function icono($Area)
+    {
+        $ruta_img = "";
+        switch ($Area) {
+            case 'Programacion':
+                $ruta_img = 'img/iconos/programacion.PNG';
+                break;
+            case 'Asignaturas Comunes':
+                $ruta_img = 'img/iconos/asignaturas-comunes.PNG';
+                break;
+            case 'Redes':
+                $ruta_img = 'img/iconos/redes.PNG';
+                break;
+            case 'Bases de datos':
+                $ruta_img = 'img/iconos/bases-datos.PNG';
+                break;
+            case 'Electronica':
+                $ruta_img = 'img/iconos/electronica.PNG';
+                break;
+            case 'Ingenieria':
+                $ruta_img = 'img/iconos/ingenieria.PNG';
+                break;
+            case 'Sistemas operativos':
+                $ruta_img = 'img/iconos/sistemasop.PNG';
+                break;
+            case 'Calculo':
+                $ruta_img = 'img/iconos/calculo.PNG';
+                break;
+            case 'Estadistica':
+                $ruta_img = 'img/iconos/estadistica.PNG';
+                break;
+            case 'Automatas':
+                $ruta_img = 'img/iconos/automatas.PNG';
+                break;
+            case 'Investigacion':
+                $ruta_img = 'img/iconos/investigacion.PNG';
+                break;
+            case 'Matematicas Aplicada':
+                $ruta_img = 'img/iconos/algebra.PNG';
+                break;
+            case 'Web':
+                $ruta_img = 'img/iconos/programacion-web.PNG';
+                break;
+            case 'Administracion':
+                $ruta_img = 'img/iconos/administracion.PNG';
+                break;
+            case 'IA':
+                $ruta_img = 'img/iconos/inteligencia-artificial.PNG';
+                break;
+            default:
+                $ruta_img = 'img/extraescolares/circuloDeLectura.PNG';
+                break;
+        }
+        return $ruta_img;
+    }
+
+    function imprimir($NumeroSemestre)
+    {
+        $cn = $this->connection;
+        $sqlQ = "SELECT * FROM tbl_materia WHERE semestre=$NumeroSemestre AND especialidadId IS NULL AND carreraId=" . $GLOBALS['carreraID'] . ";";
+        $ResultSet = $cn->query($sqlQ);
+
+        $tabla = "";
+
+        if ($ResultSet->num_rows > 0) {
+
+            $tabla .= "<div class='row justify-content-md-start justify-content-center'>";
+            while ($row = $ResultSet->fetch_assoc()) {
+                $materiaId = $row['materiaId'];
+                $nombre = $row['nombre'];
+                $competencia = $row['competencia'];
+                $area = $row['area'];
+                $urlVideo = $row['urlVideo'];
+                $urlPrograma = $row['urlPrograma'];
+                $ruta_img = $this->icono($area);
+
+                // Cuadro de materia
+                $tabla .= "<div class='col-lg-4 col-md-6 col-sm-9 col-9 p-4 h-100 justify-content-center rounded-3'>
+                <div class='row azul-medio' style='height: 88px;'>
+                    <div class='d-flex justify-content-center h-100'>
+                        <h5 class='text-white align-self-center rounded-top text-center font-semibold py-3'>$nombre</h5>
+                    </div>
+                </div>
+                <div class='row bg-light overflow-hidden d-none d-sm-flex' style='height: 110px;'>
+                    <div class='col-md-3 col-12 justify-content-center align-items-start'>
+                        <div class='d-flex flex-row justify-content-center align-items-start h-100'>
+                            <img class='h-16 p-1 mt-4 ms-4' src='$ruta_img' alt=''>
+                        </div>
+                    </div>
+                    <div class='col-md-9 col-12 justify-content-center align-items-center'>
+                        <div class='d-flex flex-row justify-content-center align-items-center h-100 text-wrap'>
+                            <p class='text-sm mx-4 my-2' style='text-align: justify;'>$competencia</p>
+                        </div>
+                    </div>
+                </div>  
+                <div class='row bg-light'>
+                    <div class='col-12 my-2 justify-content-center'>
+                        <div class='d-flex p-2 justify-content-center align-items-center'>
+                        <button type='button' class='btn btn-warning font-bold' data-bs-toggle='modal'
+                            data-bs-target='#modalReticula' 
+                                    data-materia ='$nombre' 
+                                    data-videoMateria ='$urlVideo' 
+                                    data-descMateria ='$competencia'
+                                    data-urlMateria ='$urlPrograma'
+                                    data-id='$materiaId'
+                                    onclick='youtubePlay(this)'>
+                                    Ver más </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>";
+            }
+            $tabla .= "</div>";
+        }
+
+        return $tabla;
+
+        $cn->close();
+    }
+
+    function imprimir1erSemestre()
+    {
+        return $this->imprimir(1);
+    }
+
+    function imprimir2doSemestre()
+    {
+        return $this->imprimir(2);
+    }
+
+    function imprimir3erSemestre()
+    {
+        return $this->imprimir(3);
+    }
+
+    function imprimir4toSemestre()
+    {
+        return $this->imprimir(4);
+    }
+
+    function imprimir5toSemestre()
+    {
+        return $this->imprimir(5);
+    }
+
+    function imprimir6toSemestre()
+    {
+        return $this->imprimir(6);
+    }
+
+    function imprimir7moSemestre()
+    {
+        return $this->imprimir(7);
+    }
+
+    function imprimir8voSemestre()
+    {
+        return $this->imprimir(8);
+    }
+
+    function imprimir9noSemestre()
+    {
+        return $this->imprimir(9);
+    }
+
+    function imprimirEspecialidad()
+    {
+        $cn = $this->connection;
+        $sqlQ = "SELECT * FROM tbl_especialidad WHERE carreraId=" . $GLOBALS['carreraID'] . ";";
+        $especialidades = $cn->query($sqlQ);
+        $sqlQ = "SELECT * FROM tbl_materia WHERE area='Especialidad' AND carreraId=" . $GLOBALS['carreraID'] . ";";
+        $ResultSet = $cn->query($sqlQ);
+
+        $tabla = "";
+
+        if ($ResultSet->num_rows > 0) {
+            $tabla .= "<div class='row justify-content-md-start h-100 justify-content-center'>";
+            $ruta_img = ['img/iconos/concurrentes.PNG', 'img/iconos/distribuidas.PNG', 'img/iconos/concurrentes.PNG', 'img/iconos/distribuidas.PNG', 'img/iconos/blockchain.PNG', 'img/iconos/distribuidas.PNG'];
+            $i = 0;
+            while ($row = $ResultSet->fetch_assoc()) {
+                $materiaId = $row['materiaId'];
+                $nombre = $row['nombre'];
+                $competencia = $row['competencia'];
+                $area = $row['area'];
+                $urlVideo = $row['urlVideo'];
+                $urlPrograma = $row['urlPrograma'];
+
+                // Cuadro de materia
+                $tabla .= "<div class='col-lg-4 col-md-6 col-sm-9 col-9 p-4 h-100 justify-content-center rounded-3'>
+                <div class='row azul-medio' style='height: 88px;'>
+                    <div class='d-flex justify-content-center h-100'>
+                        <h5 class='text-white align-self-center rounded-top text-center font-semibold py-3'>$nombre</h5>
+                    </div>
+                </div>
+                <div class='row bg-light overflow-hidden d-none d-sm-flex' style='height: 110px;'>
+                    <div class='col-md-3 col-12 justify-content-center align-items-start'>
+                        <div class='d-flex flex-row justify-content-center align-items-start h-100'>
+                            <img class='h-16 p-1 mt-4 ms-4' src='$ruta_img[$i]' alt=''>
+                        </div>
+                    </div>
+                    <div class='col-md-9 col-12 justify-content-center align-items-center'>
+                        <div class='d-flex flex-row justify-content-center align-items-center h-100 text-wrap'>
+                            <p class='text-sm mx-4 my-2' style='text-align: justify;'>$competencia</p>
+                        </div>
+                    </div>
+                </div>  
+                <div class='row bg-light'>
+                    <div class='col-12 my-2 justify-content-center'>
+                        <div class='d-flex p-2 justify-content-center align-items-center'>
+                        <button type='button' class='btn btn-warning font-bold' data-bs-toggle='modal'
+                            data-bs-target='#modalReticula' 
+                                    data-materia ='$nombre' 
+                                    data-videoMateria ='$urlVideo' 
+                                    data-descMateria ='$competencia'
+                                    data-urlMateria ='$urlPrograma'
+                                    data-id='$materiaId'
+                                    onclick='youtubePlay(this)'>
+                                    Ver más </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>";
+                $i = $i + 1;
+            }
+            $tabla .= "</div>";
+        }
+        return $tabla;
+
+        $cn->close();
+    }
+}
---
+++ src/View/perfilEgreso-vista.php
@@ -0,0 +1,332 @@
+<!-- Portada -->
+<div class="row g-0">
+    <div class="position-relative w-100 overflow-hidden">
+        <img class="w-100 img-fluid" src="img/ITVER-2.JPG" alt="">
+        <div class="position-absolute top-50 start-50 translate-middle w-100">
+            <div class="d-flex flex-column justify-content-center align-items-center text-center">
+                <h1 class="fw-bold text-warning">Perfil de Egreso</h1>
+                <h1 class="fw-bold text-light d-md-flex d-none">Ingeniería Mecatrónica</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Fin Portada-->
+
+<!-- Objetivo general -->
+<section class="p-2">
+    <div class="row px-2 g-0">
+        <!-- <div class="col-lg-6 col-sm-12">
+           <div class="galleryContainer bg-light">
+                <div class="row g-0">
+                    <div class="col-4 px-2">
+                        <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/IMG_1335.webp" alt="">
+                        <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/IMG_1394.webp" alt="">
+                    </div>
+
+                    <div class="col-4 px-2">
+                        <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/IMG_1407.webp" alt="">
+                        <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/IMG_1337.webp" alt="">
+                    </div>
+
+                    <div class="col-4 px-2">
+                        <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/IMG_1363.webp" alt="">
+                        <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/IMG_1380.webp" alt="">
+                    </div>
+
+                </div>
+            </div> 
+        </div>-->
+        <div class="col-lg-6 col-12 p-2 shadow-sm">
+            <div class="d-flex justify-content-center align-items-center w-100 h-100">
+                <img class="img-fluid rounded" src="img/IMG_7305.JPG" alt="">
+            </div>
+        </div>
+        <div class="lightSection col-lg-6 col-12 p-4">
+            <div class="d-flex justify-content-center align-items-center w-100 h-100">
+                <div class="row g-0">
+                    <div class="col-12">
+                        <h2 class="sectionTitle text-center font-bold m-3">Objetivo general</h2>
+                        <div class="sectionSeparator"></div>
+                    </div>
+                    <div class="col-12" style="text-align: center;">
+                        <p style="text-align: justify;">
+                            Formar profesionistas en la Ingeniería Mecatrónica con capacidad analítica, crítica e innovadora que le
+                            permita diseñar, proyectar, construir y administrar equipos y sistemas mecatrónicos en el sector social,
+                            productivo y de servicios; así como integrar, operar y mantenerlos, con un compromiso ético y de
+                            calidad en un marco de desarrollo sustentable.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Fin Objetivo general -->
+
+<!-- Perfil de egreso -->
+<div class="informationContainer">
+    <div class="row g-0 align-items-center">
+        <div class="col-lg-12">
+            <!-- Puntos del perfil de egreso -->
+            <section class="lightSection bg-light p-2">
+                <h2 class="sectionTitle text-center font-bold m-3">Perfil de egreso</h2>
+                <div class="sectionSeparator"></div>
+
+                <div class="row g-0">
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/iconos/ingenieria.PNG" alt="">
+                            <p class="textoArea">Ejerce su profesión legal y responsablemente para cumplir con las normas nacionales e
+                                internacionales que apliquen.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/topicos/ing-software-icono.PNG" alt="">
+                            <p class="textoArea">Analiza, sintetiza, diseña, simula, construye e innova productos, procesos,
+                                equipos y sistemas mecatrónicos, para impactar positivamente en su entorno con
+                                una actitud investigadora, de acuerdo
+                                a las necesidades tecnológicas, sociales actuales y emergentes.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/iconos/redes.PNG" alt="">
+                            <p class="textoArea">Instala, opera, optimiza, controla y mantiene sistemas mecatrónicos
+                                integrando tecnologías mecánicas, eléctricas, electrónicas y herramientas computacionales.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/topicos/admon-bd-icono.PNG" alt="">
+                            <p class="textoArea">Planifica, evalúa, genera, administra y transfiere proyectos industriales y
+                                de carácter social para el
+                                desarrollo tecnológico del país.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/topicos/programacion-web-icono.PNG" alt="">
+                            <p class="textoArea">Participa, coordina y/o dirige grupos multidisciplinarios a través del trabajo en equipo para asegurar
+                                la calidad, eficiencia, productividad y rentabilidad en la implementación de proyectos mecatrónicos
+                                con sentido de responsabilidad de su entorno social y cultural para un desarrollo sustentable.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/iconos/liderazgo.PNG" alt="">
+                            <p class="textoArea">Posee capacidades de liderazgo, comunicación, interrelaciones personales
+                                para transmitir ideas,
+                                facilitar conocimientos y trabajar con responsabilidad colectiva para la solución de problemas y
+                                desarrollo de proyectos con un sentido crítico y autocrítico. </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/iconos/administracion.PNG" alt="">
+                            <p class="textoArea">Desarrolla proyectos con un espíritu innovador, emprendedor y
+                                comprometido con su actualización
+                                profesional continua y autónoma, para estar a la vanguardia en los cambios científicos y
+                                tecnológicos que se dan en el ejercicio de su profesión. </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6 text-center p-3">
+                        <div class="area shadow-sm p-4">
+                            <img class="imagenArea items-center" src="img/iconos/blockchain.PNG" alt="">
+                            <p class="textoArea">Interpreta información técnica de las áreas que componen
+                                la Ingeniería Mecatrónica para la transferencia, adaptación, asimilación e innovación
+                                de tecnologías de vanguardia. </p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- Fin Puntos del perfil de egreso -->
+
+        </div>
+    </div>
+</div>
+<!-- Fin Perfil de egreso-->
+
+<!-- Galeria de imagenes -->
+<section class="darkSection bg-dark p-2 px-4">
+    <div class="row mb-4 px-2 g-0">
+        <div class="darkSection bg-dark">
+            <h2 class="titleDarkSection text-center font-bold">Galería de imágenes</h2>
+            <div class="darkSectionSeparator"></div>
+        </div>
+
+        <div class="col-lg-8 col-sm-12">
+            <div class="galleryContainer bg-dark">
+                <div class="row g-0">
+                    <div class="col-md-4 px-2">
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage1">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_4446.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage2">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Venus.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage3">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/ITVER.webp" alt="">
+                        </a>
+                    </div>
+
+                    <div class="col-md-4 px-2">
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage4">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_2253.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage5">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_0456.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage6">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_0579.webp" alt="">
+                        </a>
+                    </div>
+
+                    <div class="col-md-4 px-2">
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage7">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_1110.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage8">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_1713.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage9">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_3879.webp" alt="">
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-sm-12">
+            <div class="galleryContainer bg-dark">
+                <div class="row g-0">
+                    <div class="col-12 px-2">
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage10">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_2102.webp" alt="">
+                        </a>
+                        <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage11">
+                            <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_3878.webp" alt="">
+                        </a>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- Fin Galeria de imagenes -->
+
+
+<!-- Modals de la galería-->
+<div tabindex="-1" aria-labelledby="modalImage1" aria-hidden="true" class="modal fade" id="modalImage1">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_4446.webp" alt="">
+            <p>Logo del Instituto Tecnológico de México Veracruz</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage2" aria-hidden="true" class="modal fade" id="modalImage2">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/Venus.webp" alt="">
+            <p>Estatua Venus</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage3" aria-hidden="true" class="modal fade" id="modalImage3">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/ITVER.webp" alt="">
+            <p>Letras del Instituto Tecnológico de México Veracruz</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage4" aria-hidden="true" class="modal fade" id="modalImage4">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_2253.webp" alt="">
+            <p>Áreas recreativas</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage5" aria-hidden="true" class="modal fade" id="modalImage5">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_0456.webp" alt="">
+            <p>Áreas recreativas</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage6" aria-hidden="true" class="modal fade" id="modalImage6">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_0579.webp" alt="">
+            <p>Edificio E</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage7" aria-hidden="true" class="modal fade" id="modalImage7">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_1110.webp" alt="">
+            <p>Clúster del Laboratorio de Cómputo Intensivo</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage8" aria-hidden="true" class="modal fade" id="modalImage8">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_1713.webp" alt="">
+            <p>Laboratorio de Cómputo Intensivo</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage9" aria-hidden="true" class="modal fade" id="modalImage9">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_3879.webp" alt="">
+            <p>Laboratorio de cómputo</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage10" aria-hidden="true" class="modal fade" id="modalImage10">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_2102.webp" alt="">
+            <p>Edificio U: Sistemas</p>
+        </div>
+    </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage11" aria-hidden="true" class="modal fade" id="modalImage11">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content bg-transparent text-white text-center">
+            <img src="img/IMG_3878.webp" alt="">
+            <p>Laboratorio de cómputo</p>
+        </div>
+    </div>
+</div>
+
+<!-- Fin Modals de la galería-->

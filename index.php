0bde95dc05ecf410a3ca06fd9c6af510f0b3178c
DanielaMendezAr <danielamendez0809@gmail.com>
2023-09-11 23:27:23 -0600

    update

--- index.php
+++ index.php
@@ -33,8 +33,7 @@ $comunidad->setConnection($conn->getDB());
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Ingeniería Mecátronica">
-    <meta name="author" content="Daniela Castro Rodriguez, Gabriel Escobar Medina, Nancy Daniela Mendez Arpidez,
-    Marco Gabriel Cortes Toledo, Yelitza Magali Rosas Jimenez, Ángel Manuel Sandria Pérez">
+    <meta name="author" content="Tecnológico Nacional de México Veracruz">
     <link rel="icon" href="img/itver-logo.PNG" />
 
     <link rel="stylesheet" href="assets/css/bootstrap.css">

76bb6bdb26196dc932a95ef55b8fb5d561d89b05
Daniela Castro <daniela.castro@phs.mx>
2023-10-02 18:11:21 -0600

    cambio de titulos

--- src/View/conocenos-vista.php
+++ src/View/conocenos-vista.php
@@ -92,7 +92,7 @@ $imagenCoordinador = $GLOBALS['PATH_DOCENTE'] . $coordinador['imagen'];
             <h4 class="text-center fw-bold fs-3">
               <?php
               if ($jefeDepartamento) {
-                echo $jefeDepartamento['nombrePuesto'] . " de Sistemas Computacionales";
+                echo $jefeDepartamento['nombrePuesto'] . " de Metal - Mecánica";
               }
               ?>
             </h4>
@@ -146,7 +146,7 @@ $imagenCoordinador = $GLOBALS['PATH_DOCENTE'] . $coordinador['imagen'];
             <h4 class="text-center fw-bold fs-3">
               <?php
               if ($coordinador) {
-                echo $coordinador['nombrePuesto'] . " de Sistemas Computacionales";
+                echo $coordinador['nombrePuesto'] . " de Ingeniería Mecatrónica";
               }
               ?>
             </h4>
